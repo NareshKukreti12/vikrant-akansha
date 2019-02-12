89321d53b0adc9a9eb2099f5a35f56d4b14b631b
NareshKukreti12 <kukreti12.naresh@gmail.com>
2019-02-12 18:02:52 +0530

    Small changes

--- app.js
+++ app.js
@@ -36,59 +36,7 @@ SwaggerExpress.create(config, function(err, swaggerExpress) {
   if (swaggerExpress.runner.swagger.paths['/hello']) {
     //console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
   
-  }
-
-
-  // console.log("IO",io)
-
-  // io.on('connection',(socket)=>{
-  //   socket.on('join',(params,callback)=>{
-  //      if(!isRealString(params.name) || (!isRealString(params.room))){
-  //       return callback('Name and room name are required');
-  //      }
-  //      socket.join(params.room);
-  //      users.removeUser(socket.id);
-  //      users.addUser(socket.id,params.name,params.room)
-
-  //      io.to(params.room).emit('updateUserList',users.getUserList(params.room));
-
-  //      //socket.leave('');
-  //      //socket.broadcast.emit
-  //      //socket.emit
-  //      socket.emit('newMessage',generateMessage('Admin','Welcome to the chat app'));
-  //      socket.broadcast.to(params.room).emit('newMessage',generateMessage('Admin',`${params.name} has joined`));
-  //      callback()
-  //   })
-  //   socket.on('createMessage',(message,callback)=>{
-        
-  //    var user=users.getUser(socket.id);
-  //    if(user && isRealString(message.text)){
-  //      io.to(user.room).emit('newMessage',generateMessage(user.name,message.text))
-  //    }
-
-  //      // io.emit('newMessage',generateMessage(message.from,message.text))
-  //       callback('This is from the server.');
-  //   })
-  //   socket.on('createLocationMessage',(coords)=>{
-  //     var user=users.getUser(socket.id);
-  //     if(user){
-  //       io.to(user.room).emit('newLocationMessage',generateLocationMessage(user.name,coords.latitude,coords.longitude))
-  //     } 
-     
-  //   })
-
-  //   socket.on('disconnect',()=>{
-  //      var user=users.removeUser(socket.id);
-  //      if(user){
-  //        io.to(user.room).emit('updateUserList',users.getUserList(user.room));
-  //        io.to(user.room).emit('newMessage',generateMessage('Admin',`${user.name} has left`));
-  //      }
-  //   })
-  //   socket.on('create',(newEmail)=>{
-  //       console.log("Create Email",   newEmail);
-  //   })
-  // })
-
+  } 
 });
 
 
