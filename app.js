7a8056904e4a028ebb4a9faef4d3873852922116
NareshKukreti12 <kukreti12.naresh@gmail.com>
2019-02-12 18:28:27 +0530

    New commit

--- app.js
+++ app.js
@@ -33,10 +33,9 @@ SwaggerExpress.create(config, function(err, swaggerExpress) {
  
 
 
-  if (swaggerExpress.runner.swagger.paths['/hello']) {
-    //console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
+  // if (swaggerExpress.runner.swagger.paths['/hello']) {
   
-  } 
+  // } 
 });
 
 
